025db6f270c8dd3b0b5d6414118134b3d579a2ea
MagicAhn <rogerowen@qq.com>
2013-12-12 17:32:47 +0800

    Scripts

--- MecanimLearn/Assets/Locomotion/Scripts/Locomotion.cs
+++ MecanimLearn/Assets/Locomotion/Scripts/Locomotion.cs
@@ -1,44 +1,62 @@
+﻿using System;
 using UnityEngine;
 using System.Collections;
 
-public class Locomotion
+/// <summary>
+/// Locomotion 的逻辑
+/// </summary>
+public class Locomotion : MonoBehaviour
 {
     private Animator m_Animator = null;
-    
-    private int m_SpeedId = 0;
-    private int m_AgularSpeedId = 0;
-    private int m_DirectionId = 0;
+    private Int32 m_SpeedId = 0;
+    private Int32 m_AngularSpeedId = 0;
+    private Int32 m_DirectionId = 0;
 
     public float m_SpeedDampTime = 0.1f;
     public float m_AnguarSpeedDampTime = 0.25f;
     public float m_DirectionResponseTime = 0.2f;
-    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     public Locomotion(Animator animator)
     {
         m_Animator = animator;
 
+        // 将 游戏中 附加到Player的 Animator的Controller 中设置的 Parameters 转换成一个 Id
         m_SpeedId = Animator.StringToHash("Speed");
-        m_AgularSpeedId = Animator.StringToHash("AngularSpeed");
+        m_AngularSpeedId = Animator.StringToHash("AngularSpeed");
         m_DirectionId = Animator.StringToHash("Direction");
     }
 
-    public void Do(float speed, float direction)
+    public void Do(Single speed, Single direction)
     {
+        // Base Layer 的 StateInformation
         AnimatorStateInfo state = m_Animator.GetCurrentAnimatorStateInfo(0);
 
-        bool inTransition = m_Animator.IsInTransition(0);
-        bool inIdle = state.IsName("Locomotion.Idle");
-        bool inTurn = state.IsName("Locomotion.TurnOnSpot") || state.IsName("Locomotion.PlantNTurnLeft") || state.IsName("Locomotion.PlantNTurnRight");
-        bool inWalkRun = state.IsName("Locomotion.WalkRun");
+        Boolean inTransition = m_Animator.IsInTransition(0);
+        // 判断 Animator State Machine的 活动状态的 Name和给定的 name匹配 
+        Boolean inIdle = state.IsName("Locomotion.Idle");
+        Boolean inTurn = state.IsName("Locomotion.TurnOnSpot") || state.IsName("Locomotion.PlantNTurnRight") || state.IsName("Locomotion.PlantNTurnLeft");
+        Boolean inWalkRun = state.IsName("Locomotion.WalkRun");
 
-        float speedDampTime = inIdle ? 0 : m_SpeedDampTime;
-        float angularSpeedDampTime = inWalkRun || inTransition ? m_AnguarSpeedDampTime : 0;
-        float directionDampTime = inTurn || inTransition ? 1000000 : 0;
+        Single speedDampTime = inIdle ? 0 : m_SpeedDampTime;
+        Single angularSpeedDampTime = inWalkRun || inTransition ? m_AnguarSpeedDampTime : 0;
+        Single directionDampTime = inTurn || inTransition ? 1000000 : 0;
 
-        float angularSpeed = direction / m_DirectionResponseTime;
-        
+        Single angularSpeed = direction / m_DirectionResponseTime;
+
         m_Animator.SetFloat(m_SpeedId, speed, speedDampTime, Time.deltaTime);
-        m_Animator.SetFloat(m_AgularSpeedId, angularSpeed, angularSpeedDampTime, Time.deltaTime);
+        m_Animator.SetFloat(m_AngularSpeedId, angularSpeed, angularSpeedDampTime, Time.deltaTime);
         m_Animator.SetFloat(m_DirectionId, direction, directionDampTime, Time.deltaTime);
-    }	
+    }
 }
--- MecanimLearn/Assets/Scripts/JoystickToEvents.cs
+++ MecanimLearn/Assets/Scripts/JoystickToEvents.cs
@@ -1,37 +1,62 @@
+﻿using System;
 using UnityEngine;
 using System.Collections;
 
-public class JoystickToEvents : MonoBehaviour 
+/// <summary>
+/// 根据 Joystick 的输入 得到 Player 运动的速度和方向
+/// </summary>
+public class JoystickToEvents : MonoBehaviour
 {
-    public static void Do(Transform root, Transform camera, ref float speed, ref float direction)
+
+    // Use this for initialization
+    void Start()
     {
-        Vector3 rootDirection = root.forward;
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-				
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// 接受 用户输入 ，计算出 速率和方向 的 数值，通过数值来控制动画播放
+    /// </summary>
+    /// <param name="root">Player 的 transform</param>
+    /// <param name="camera">Camera 的 transform</param>
+    /// <param name="speed">Player 的 Speed（用来控制Player的Animation 是Idle，Walk还是Run）</param>
+    /// <param name="direction">Player 的 direction（用来控制 Player的 Animation 如何转弯）</param>
+    public static void Do(Transform root, Transform camera, out Single speed, out Single direction)
+    {
+        // 得到 Joystick的 方向
+        Single horizontal = Input.GetAxis("Horizontal");
+        Single vertical = Input.GetAxis("Vertical");
+
         Vector3 stickDirection = new Vector3(horizontal, 0, vertical);
 
-        // Get camera rotation.    
+        // 取 Player和Camera的 正前方的 direction
+        Vector3 rootDirection = root.forward;
+        Vector3 cameraDirection = camera.forward;
+        // 把 Camera在y轴 方向的位置 设置为零，得到 在 xy面上的 向量
+        Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
 
-        Vector3 CameraDirection = camera.forward;
-        CameraDirection.y = 0.0f; // kill Y
-        Quaternion referentialShift = Quaternion.FromToRotation(Vector3.forward, CameraDirection);
-
-        // Convert joystick input in Worldspace coordinates
+        // 将 Joystick的输入 转换成 世界坐标系中 坐标
         Vector3 moveDirection = referentialShift * stickDirection;
-				
-		Vector2 speedVec =  new Vector2(horizontal, vertical);
-		speed = Mathf.Clamp(speedVec.magnitude, 0, 1);      
 
-        if (speed > 0.01f) // dead zone
+        // Joystick 输入的值 作为 Player 在 xy平面上的 速度的方向
+        Vector2 speedVec = new Vector2(horizontal, vertical);
+        speed = Mathf.Clamp(speedVec.magnitude, 0, 1);
+
+        // 判断是否为死区
+        if (speed > 0.01f)
         {
-			Vector3 axis = Vector3.Cross(rootDirection, moveDirection);
-			direction = Vector3.Angle(rootDirection, moveDirection) / 180.0f * (axis.y < 0 ? -1 : 1);
+            Vector3 axis = Vector3.Cross(rootDirection, moveDirection);
+            direction = Vector3.Angle(rootDirection, moveDirection) / 180.0f * (axis.y < 0 ? -1 : 1);
         }
         else
-		{
+        {
             direction = 0.0f;
-		}
+        }
     }
-	
 }
